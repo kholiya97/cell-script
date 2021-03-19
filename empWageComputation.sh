3c6e671c79019b822c9ac049ae3b2693854c62e1
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-03-20 00:24:04 +0530

    modified add partime wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,33 @@
-echo "welcome"
+sFullTime=1
+isPartTime=2
+isAbsent=0
+isPresent=1
+fullTimeHr=16
+partTimeHr=8
+salaryPerHr=20
+salary=0
+totalSalary=0
 
-isPresent=1;
-RandomCheck=$(( $RANDOM%2 ));
-if [ $isPresent -eq $RandomCheck ];
+randomCheck=$(($RANDOM%3))
+
+
+
+if [ $isPresent -eq $randomCheck ]
 then
-        EmpRatePerHr=20;
-        EmpHrs=8;
-Salary=$(($EmpHrs*$EmpRatePerHr));
+echo "Employe Present"
 else
-Salary=0;
+echo "Employee Absent"
 fi
-echo $Salary
+
+if [ $randomCheck -eq $isFullTime ]
+then
+        salary=$(($fullTimeHr*$salaryPerHr))
+elif [ $randomCheck -eq $isPartTime ]
+then
+        salary=$(($partTimeHr*$salaryPerHr))
+else
+        salary=0
+fi
+
+echo $salary
+#echo  $randomCheck
