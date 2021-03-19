db35bbe4f727a1713807791c8fd69dac7bae937b
kholiya97 <80846923+kholiya97@users.noreply.github.com>
2021-03-19 23:53:43 +0530

    day 7

---
+++ day7/10random3digitno.sh
@@ -0,0 +1,24 @@
+$ cat 10random3digitno.sh
+#!/bin/bash
+for((i=0 ;i<10;i++))
+do
+random=$((100 + RANDOM%20))
+num[$i]=$random
+done
+echo "Array Elements :-" ${num[@]}
+temp=0
+for (( i=0; i<10; i++))
+do
+for ((j=i+1; j<10 ; j++))
+do
+if [ ${num[i]} -gt ${num[$((j))]} ]
+then
+temp=${num[i]}
+num[$i]=${num[$((j))]}
+num[$((j))]=$temp
+fi
+done
+done
+##echo "Array After Sorting :- { ${num[@]} }"
+echo "Second Smallest :- ${num[1]}"
+echo "Second Largest :- ${num[9]}"
